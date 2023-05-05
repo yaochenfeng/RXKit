f7395cd571d4d736156ef74b8e0f1082232ce9dc
yaochenfeng <yaochenfeng@1024ul.com>
2023-05-05 13:36:19 +0800

    chore: 目录结构

--- Sources/RXKit/Extension/Rx+View.swift
+++ Sources/RXKit/Extension/Rx+View.swift
@@ -4,6 +4,53 @@
 //
 //  Created by yaochenfeng on 2023/1/16.
 //
+
+#if canImport(SnapKit)
+import SnapKit
+/// 转换UIView协议
+public protocol RXViewConvertible {
+    /// 转为视图
+    /// - Returns: UIView
+    func asView() -> ConstraintView
+}
+
+extension ConstraintView: RXViewConvertible {
+    public func asView() -> ConstraintView {
+        return self
+    }
+}
+
+public extension Reactive where Base: ConstraintView {
+    /// SnapKit DSL
+    var snp: ConstraintViewDSL {
+        return base.snp
+    }
+    
+    /// 添加到父视图
+    /// - Parameter sp: 父视图
+    /// - Returns: Reactive
+    @discardableResult
+    func add2(_ sp: RXViewConvertible) -> Self {
+        sp.asView().addSubview(base)
+        return self
+    }
+    /// snapkit make布局
+    /// - Parameter mk: 布局描述
+    /// - Returns: 自身(for chainable)
+    @discardableResult
+    func layout(_ mk: (ConstraintMaker) -> Void) -> Self {
+        snp.makeConstraints(mk)
+        return self
+    }
+}
+
+extension Reactive: RXViewConvertible where Base: ConstraintView {
+    public func asView() -> ConstraintView {
+        return base
+    }
+}
+#endif
+
 #if canImport(UIKit)
 import UIKit
 /// 通用UIView拓展
