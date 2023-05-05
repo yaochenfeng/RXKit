43f6c740737b9f2c3a7dbb2de8d67515711f59f5
yaochenfeng <yaochenfeng@1024ul.com>
2023-05-05 15:56:33 +0800

    chore: 文件目录

--- Sources/RXKit/Route/RXNavigationController.swift
+++ Sources/RXKit/Route/RXNavigationController.swift
@@ -8,35 +8,6 @@
 #if canImport(UIKit)
 import UIKit
 
-/// 路由转换协议
-public protocol RXRouteConvertible {
-    /// 字符串转路由
-    init?(string: String)
-    /// 路由ID
-    var routeId: String { get }
-    /// 路由转控制器
-    func getController() -> UIViewController?
-    /// 获取导航控制器
-    static func getNavigationController() -> UINavigationController
-    /// 导航到对应路由
-    func navigate(animated: Bool, source: UIViewController?)
-}
-public extension RXRouteConvertible {
-    /// 通用导航控制器
-    static func getNavigationController() -> UINavigationController {
-        return RXNavigationController.shared
-    }
-    /// 默认使用push
-    func navigate(animated: Bool = true, source: UIViewController? = nil) {
-        let nav = Self.getNavigationController()
-        if let old = nav.rx.getRouteController(routeId) {
-            nav.popToViewController(old, animated: animated)
-        } else if let vc = getController() {
-            nav.pushViewController(vc, animated: animated)
-        }
-    }
-    
-}
 /// 通用UIViewController拓展
 public extension Reactive where Base: UINavigationController {
     /// 是否已经包含某个路由
